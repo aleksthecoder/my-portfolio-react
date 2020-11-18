3b18dd9c4a2a1ebedfb0e4780bc1f14139f2f749
Aleks <69213547+aleksthecoder@users.noreply.github.com>
2020-11-18 11:14:12 +0000

    Removed logo animation.

--- src/componenets/Logo.js
+++ src/componenets/Logo.js
@@ -1,20 +1,10 @@
-import Icon from './Icon'
 import '../stylesheets/logo.css'
 
 function Logo(props) {
     return (
         <div className='logo'>
             <div className='logoText'>
-            c
-            </div>
-            <div className='logoIcon'>
-                <Icon
-                    iconName={props.iconName}
-                    iconPrefix={props.iconPrefix}
-                />
-            </div>
-            <div className='logoText'>
-                dealeks
+            codealeks
             </div>
         </div>
         
